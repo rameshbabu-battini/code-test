6ea866450e57efe1f1caedc2c9c568fb4fea49cb
Ramesh Babu Battini <rameshblore4u@gmail.com>
2019-11-10 00:38:58 +0530

    Changes for exercise 2

--- src/main/com/ge/exercise2/ArrayQuadrantUtil.java
+++ src/main/com/ge/exercise2/ArrayQuadrantUtil.java
@@ -1,18 +1,80 @@
 package com.ge.exercise2;
 
+import java.lang.reflect.Array;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ArrayQuadrantUtil {
-    private static final Logger logger = LogManager.getLogger(ArrayQuadrantUtil.class);
+public class ArrayQuadrantUtil<T> {
+	private static final Logger logger = LogManager.getLogger(ArrayQuadrantUtil.class);
 
-    char[][] data;
+	T[][] data;
+	Class<?> classType;
+	int rowSize;
+	int columnSize;
 
-    public ArrayQuadrantUtil(char[][] data) {
-        this.data = data;
-    }
+	public ArrayQuadrantUtil(T[][] data) {
+		this.data = data;
+		if (data != null) {
+			this.classType = data[0][0].getClass();
+			this.rowSize = data.length;
+			this.columnSize = data[0].length;
+		}
+	}
 
-    public char[] getQuadrantValues(int row, int column) {
-        return null;
-    }
+	@SuppressWarnings("unchecked")
+	public T[] getQuadrantValues(int row, int column, int partitionSize) {
+		if (data != null) {
+			for (int i = 0; i < rowSize; i++) {
+				for (int j = 0; j < columnSize; j++) {
+					if (i == row && j == column) {
+						T[] quadrantValues = (T[]) Array.newInstance(classType, partitionSize * partitionSize);
+						int index = 0;
+						for (int l = i; l < i + partitionSize; l++) {
+							for (int m = j; m < j + partitionSize; m++) {
+								quadrantValues[index++] = data[l][m];
+							}
+						}
+						return quadrantValues;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T[] getRowValues(int row) {
+		if (data != null) {
+			for (int i = 0; i < rowSize; i++) {
+				if (i == row) {
+					T[] quadrantValues = (T[]) Array.newInstance(classType, columnSize);
+					for (int j = 0; j < columnSize; j++) {
+						quadrantValues[j] = data[i][j];
+					}
+					return quadrantValues;
+				}
+			}
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T[] getColumnValues(int column) {
+		if (data != null) {
+			for (int i = 0; i < rowSize; i++) {
+				for (int j = 0; j < columnSize; j++) {
+					if (i == column) {
+						T[] quadrantValues = (T[]) Array.newInstance(classType, rowSize);
+						for (int k = 0; k < rowSize; k++) {
+							quadrantValues[j] = data[k][j];
+						}
+						return quadrantValues;
+					}
+				}
+
+			}
+		}
+		return null;
+	}
 }
--- src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
+++ src/test/com/ge/exercise2/ArrayQuadrantUtilTest.java
@@ -1,25 +1,74 @@
 package com.ge.exercise2;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Assume;
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
 
 public class ArrayQuadrantUtilTest {
 
     @Test
     public void getQuadrantValuesTest() {
-        char[][] data = {
+        Character[][] data = {
                 {'a', 'b', 'c', 'd'},
                 {'e', 'f', 'g', 'h'},
                 {'i', 'j', 'k', 'l'},
                 {'m', 'n', 'o', 'p'}
         };
 
-        ArrayQuadrantUtil util = new ArrayQuadrantUtil(data);
-        Assume.assumeNotNull(util.getQuadrantValues(0, 0));
+        ArrayQuadrantUtil<Character> util = new ArrayQuadrantUtil<>(data);
+        Assume.assumeNotNull((Object[])util.getQuadrantValues(0, 0, 2));
 
-        char[] expectedResult = {'a', 'b', 'e', 'f'};
-        assertArrayEquals(expectedResult, util.getQuadrantValues(0, 0));
+        Character[] expectedResult = {'a', 'b', 'e', 'f'};
+        assertArrayEquals(expectedResult, util.getQuadrantValues(0, 0, 2));
     }
+    
+    @Test
+    public void getRowValuesTest() {
+        Character[][] data = {
+                {'a', 'b', 'c', 'd'},
+                {'e', 'f', 'g', 'h'},
+                {'i', 'j', 'k', 'l'},
+                {'m', 'n', 'o', 'p'}
+        };
+
+        ArrayQuadrantUtil<Character> util = new ArrayQuadrantUtil<>(data);
+        Assume.assumeNotNull((Object[])util.getRowValues(1));
+
+        Character[] expectedResult = {'e', 'f', 'g', 'h'};
+        assertArrayEquals(expectedResult, util.getRowValues(1));
+    }
+    
+    @Test
+    public void getColumnValuesTest() {
+        Character[][] data = {
+                {'a', 'b', 'c', 'd'},
+                {'e', 'f', 'g', 'h'},
+                {'i', 'j', 'k', 'l'},
+                {'m', 'n', 'o', 'p'}
+        };
+
+        ArrayQuadrantUtil<Character> util = new ArrayQuadrantUtil<>(data);
+        Assume.assumeNotNull((Object[])util.getColumnValues(2));
+
+        Character[] expectedResult = {'c', 'g', 'k', 'o'};
+        assertArrayEquals(expectedResult, util.getColumnValues(2));
+    }
+    
+    @Test
+    public void getQuadrantValuesTest2() {
+        Integer[][] data = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        ArrayQuadrantUtil<Integer> util = new ArrayQuadrantUtil<>(data);
+        Assume.assumeNotNull((Object[])util.getQuadrantValues(1, 1, 2));
+
+        Integer[] expectedResult = {6, 7, 10, 11};
+        assertArrayEquals(expectedResult, util.getQuadrantValues(1, 1, 2));
+    }
+    
 }
